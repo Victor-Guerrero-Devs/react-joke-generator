97bbbb244c6133743e9834fd0a36bdaf877225ce
Victor-Guerrero-Devs <Vicguerrero.dev@gmail.com>
2023-02-05 22:59:28 -0600

    Add fetchJoke func

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import { useState } from 'react';
 
 function App() {
-  return <button>Telle me a joke</button>;
+  async function fetchJoke() {
+    const url = 'https://icanhazdadjoke.com/';
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  return (
+    <>
+      <h1>Joke Generator</h1>
+      <button onClick={fetchJoke}>Get Joke</button>
+    </>
+  );
 }
 
 export default App;
